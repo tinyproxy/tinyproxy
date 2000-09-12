95ac302b8f4f60a9522abb0b4cd7846a69023d60
Robert James Kaes <rjkaes@users.sourceforge.net>
2000-09-12 00:18:17 +0000

    New defaults, and better checking for various features needed by tinyproxy. Looks like fun! :)

--- acconfig.h
+++ acconfig.h
@@ -2,7 +2,7 @@
  * Define if you want to have the peer's IP address to be included in a
  * XTinyproxy header sent to the server.
  */
-#undef XTINYPROXY
+#undef XTINYPROXY_ENABLE
 
 /*
  * These are the defaults, but they can be changed by configure at compile
@@ -11,6 +11,7 @@
 #define DEFAULT_LOG "/var/log/tinyproxy"
 #define DEFAULT_PORT 8888
 #define DEFAULT_USER ""
+#define DEFAULT_CONF_FILE "/etc/tinyproxy/tinyproxy.conf"
 
 /*
  * Define if you would like to include filtering code.
@@ -25,4 +26,32 @@
 /*
  * Define if you want to include upstream proxy support
  */
-#undef UPSTREAM_PROXY
+#undef TUNNEL_SUPPORT
+
+/*
+ * NOTE: for DEFAULT_STATHOST:  this controls remote proxy stats display.
+ * for example, the default DEFAULT_STATHOST of "tinyproxy.stats" will
+ * mean that when you use the proxy to access http://tinyproxy.stats/",
+ * you will be shown the proxy stats.  Set this to something obscure
+ * if you don't want random people to be able to see them, or set it to
+ * "" to disable.  In the future, I figure maybe some sort of auth
+ * might be desirable, but that would involve a major simplicity
+ * sacrifice.
+ *
+ *
+ * The "hostname" for getting tinyproxy stats. "" = disabled by default
+ */
+#define DEFAULT_STATHOST "tinyproxy.stats"
+
+/*
+ * Define the following for the appropriate datatype, if necessary
+ */
+#undef uint8_t
+#undef int16_t
+#undef uint16_t
+#undef int32_t
+#undef uint32_t
+#undef in_addr_t 
+#undef size_t
+#undef ssize_t
+#undef socklen_t
