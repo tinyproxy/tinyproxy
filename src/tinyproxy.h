f8b61da4ae5597f02c78c48f429c93d9024034df
Robert James Kaes <rjkaes@users.sourceforge.net>
2001-11-25 22:07:36 +0000

    Added tests for _every_ header to make sure they are actually present on the system.

--- src/tinyproxy.h
+++ src/tinyproxy.h
@@ -1,4 +1,4 @@
-/* $Id: tinyproxy.h,v 1.20 2001-11-22 00:31:10 rjkaes Exp $
+/* $Id: tinyproxy.h,v 1.21 2001-11-25 22:07:36 rjkaes Exp $
  *
  * See 'tinyproxy.c' for a detailed description.
  *
@@ -26,52 +26,133 @@
 /*
  * Include standard headers which are used through-out tinyproxy
  */
-#include        <sys/types.h>
+#  include      <sys/types.h>
+#ifdef HAVE_INTTYPES_H
+#  include	<inttypes.h>
+#endif
+#ifdef HAVE_STDDEF_H
+#  include	<stddef.h>
+#endif
+#ifdef HAVE_STDINT_H
+#  include	<stdint.h>
+#endif
+
+#ifdef HAVE_SYS_IOCTL_H
+#  include	<sys/ioctl.h>
+#endif
 #ifdef HAVE_SYS_RESOURCE_H
 #  include      <sys/resource.h>
 #endif
 #ifdef HAVE_SYS_SELECT_H
 #  include	<sys/select.h>
 #endif
-#include	<sys/socket.h>
-#include	<sys/stat.h>
-#ifdef TIME_WITH_SYS_TIME
+#ifdef HAVE_SYS_SOCKET_H
+#  include	<sys/socket.h>
+#endif
+#ifdef HAVE_SYS_STAT_H
+#  include	<sys/stat.h>
+#endif
+#ifdef HAVE_SYS_SYSCTL_H
+#  include	<sys/sysctl.h>
+#endif
+
+#ifdef HAVE_SYS_TIME_H
 #  include	<sys/time.h>
-#  include	<time.h>
+#  ifdef HAVE_TIME_H
+#    include	<time.h>
+#  endif
 #else
 #  ifdef HAVE_SYS_TIME_H
 #    include 	<sys/time.h>
 #  else
-#    include	<time.h>
+#    ifdef HAVE_TIME_H
+#      include	<time.h>
+#    endif
 #  endif
 #endif
-#include	<sys/uio.h>
-#include	<netinet/in.h>
-#include	<arpa/inet.h>
-#include        <assert.h>
-#include        <ctype.h>
-#include	<errno.h>
-#include	<fcntl.h>
-#include        <grp.h>
-#include	<netdb.h>
+
+#ifdef HAVE_SYS_UIO_H
+#  include	<sys/uio.h>
+#endif
+#ifdef HAVE_SYS_WAIT_H
+#  include	<sys/wait.h>
+#endif
+
+#ifdef HAVE_NETINET_IN_H
+#  include	<netinet/in.h>
+#endif
+#ifdef HAVE_ARPA_INET_H
+#  include	<arpa/inet.h>
+#endif
+#ifdef HAVE_ALLOCA_H
+#  include	<alloca.h>
+#endif
+#ifdef HAVE_ASSERT_H
+#  include      <assert.h>
+#endif
+#ifdef HAVE_CTYPE_H
+#  include      <ctype.h>
+#endif
+#ifdef HAVE_ERRNO_H
+#  include	<errno.h>
+#endif
+#ifdef HAVE_FCNTL_H
+#  include	<fcntl.h>
+#endif
+#ifdef HAVE_GRP_H
+#  include    	<grp.h>
+#endif
+#ifdef HAVE_MALLOC_H
+#  include	<malloc.h>
+#endif
+#ifdef HAVE_MEMORY_H
+#  include	<memory.h>
+#endif
+#ifdef HAVE_NETDB_H
+#  include	<netdb.h>
+#endif
 #ifdef HAVE_PTHREAD_H
 #  include	<pthread.h>
 #endif
-#include        <pwd.h>
-#include        <signal.h>
-#include        <stdarg.h>
-#ifdef HAVE_STDINT_H
-#  include	<stdint.h>
+#ifdef HAVE_PWD_H
+#  include     	<pwd.h>
 #endif
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<string.h>
+#ifdef HAVE_SIGNAL_H
+#  include      <signal.h>
+#endif
+#ifdef HAVE_STDARG_H
+#  include      <stdarg.h>
+#endif
+#ifdef HAVE_STDIO_H
+#  include	<stdio.h>
+#endif
+#ifdef HAVE_STDLIB_H
+#  include	<stdlib.h>
+#endif
+#ifdef HAVE_STRING_H
+#  include	<string.h>
+#endif
 #ifdef HAVE_STRINGS_H
 #  include	<strings.h>
 #endif
-#include        <sysexits.h>
-#include        <syslog.h>
-#include        <unistd.h>
+#ifdef HAVE_SYSEXITS_H
+#  include     	<sysexits.h>
+#endif
+#ifdef HAVE_SYSLOG_H
+#  include      <syslog.h>
+#endif
+#ifdef HAVE_UNISTD_H
+#  include      <unistd.h>
+#endif
+#ifdef HAVE_VFORK_H
+#  include	<vfork.h>
+#endif
+#ifdef HAVE_WCHAR_H
+#  include	<wchar.h>
+#endif
+#ifdef HAVE_WCTYPE_H
+#  include	<wctype.h>
+#endif
 
 #ifndef SHUT_RD			/* these three Posix.1g names are quite new */
 #  define SHUT_RD	0	/* shutdown for reading */
