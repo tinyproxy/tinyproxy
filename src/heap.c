61f8ab527c2915a1e076b2c6386a0589b5d60aa3
Mukund Sivaraman <muks@banu.com>
2008-06-19 20:50:10 +0530

    Fix format strings for size_t

--- src/heap.c
+++ src/heap.c
@@ -33,8 +33,8 @@ debugging_calloc(size_t nmemb, size_t size, const char *file,
 	assert(size > 0);
 
 	ptr = calloc(nmemb, size);
-	fprintf(stderr, "{calloc: %p:%u x %u} %s:%lu\n", ptr, nmemb, size, file,
-		line);
+	fprintf(stderr, "{calloc: %p:%zu x %zu} %s:%lu\n",
+                ptr, nmemb, size, file, line);
 	return ptr;
 }
 
@@ -46,7 +46,8 @@ debugging_malloc(size_t size, const char *file, unsigned long line)
 	assert(size > 0);
 
 	ptr = malloc(size);
-	fprintf(stderr, "{malloc: %p:%u} %s:%lu\n", ptr, size, file, line);
+	fprintf(stderr, "{malloc: %p:%zu} %s:%lu\n",
+                ptr, size, file, line);
 	return ptr;
 }
 
@@ -54,12 +55,12 @@ void *
 debugging_realloc(void *ptr, size_t size, const char *file, unsigned long line)
 {
 	void *newptr;
-	
+
 	assert(size > 0);
-	
+
 	newptr = realloc(ptr, size);
-	fprintf(stderr, "{realloc: %p -> %p:%u} %s:%lu\n", ptr, newptr, size,
-		file, line);
+	fprintf(stderr, "{realloc: %p -> %p:%zu} %s:%lu\n",
+                ptr, newptr, size, file, line);
 	return newptr;
 }
 
@@ -74,7 +75,7 @@ debugging_free(void *ptr, const char *file, unsigned long line)
 }
 
 char*
-debugging_strdup(const char* s, const char* file, unsigned long line)
+debugging_strdup(const char *s, const char *file, unsigned long line)
 {
 	char* ptr;
 	size_t len;
@@ -87,7 +88,8 @@ debugging_strdup(const char* s, const char* file, unsigned long line)
 		return NULL;
 	memcpy(ptr, s, len);
 
-	fprintf(stderr, "{strdup: %p:%u} %s:%lu\n", ptr, len, file, line);
+	fprintf(stderr, "{strdup: %p:%zu} %s:%lu\n",
+                ptr, len, file, line);
 	return ptr;
 }
 
