tinyproxy/tinyproxy

commit 95c1f39f6039dc82346f3e024e86a23b7103a0a6

Don't check the value to be inserted as it's already checked
This change primarily avoids a gcc warning where timebuf
is never non-NULL. There is no need to check the value to be
inserted as it's checked inside hashmap_insert().

This changeset also lets error return values from hashmap_insert()
propogate instead of clamping them to -1 (not that these are
currently used anyway).

src/html-error.c
@@ -232,14 +232,15 @@ add_error_variable(struct conn_s *connptr, char *key, char *val)
                      hashmap_create(ERRVAR_BUCKETCOUNT)))
                         return (-1);
 
-        if (hashmap_insert(connptr->error_variables, key, val,
-                           strlen(val) + 1) < 0)
-                return (-1);
-
-        return (0);
+        return hashmap_insert(connptr->error_variables, key, val,
+			      strlen(val) + 1);
 }
 
-#define ADD_VAR_RET(x, y) if(y) { if(add_error_variable(connptr, x, y) == -1) return(-1); }
+#define ADD_VAR_RET(x, y)				   \
+	do {						   \
+		if (add_error_variable(connptr, x, y) < 0) \
+			return -1;			   \
+	} while (0)
 
 /*
  * Set some standard variables used by all HTML pages

commit 7e5ac7c58a7a301d9c6d65bbc2c3632fc0fba5ed

Fix a regression where empty error variables caused strlen() to crash
This fixes a regression (bug #16) introduced in
95c1f39f6039dc82346f3e024e86a23b7103a0a6, where a NULL check was
removed. This caused NULL error variable values to be sent to
add_error_variable() in which strlen() segfaulted.

With this fix, custom stats pages should be displayed properly.

X-Banu-Bugzilla-Ids: 16

src/html-error.c
@@ -244,7 +244,9 @@ add_error_variable(struct conn_s *connptr, char *key, char *val)
 }
 
 #define ADD_VAR_RET(x, y)				   \
-	do {						   \
+	do {                                               \
+                if (y == NULL)                             \
+                        break;                             \
 		if (add_error_variable(connptr, x, y) < 0) \
 			return -1;			   \
 	} while (0)
@@ -266,14 +268,20 @@ add_standard_vars(struct conn_s *connptr)
         ADD_VAR_RET("request", connptr->request_line);
         ADD_VAR_RET("clientip", connptr->client_ip_addr);
         ADD_VAR_RET("clienthost", connptr->client_string_addr);
-        ADD_VAR_RET("version", VERSION);
-        ADD_VAR_RET("package", PACKAGE);
-        ADD_VAR_RET("website", "http://tinyproxy.banu.com/");
+
+        /* The following value parts are all non-NULL and will
+         * trigger warnings in ADD_VAR_RET(), so we use
+         * add_error_variable() directly.
+         */
 
         global_time = time(NULL);
         strftime(timebuf, sizeof(timebuf), "%a, %d %b %Y %H:%M:%S GMT",
                  gmtime(&global_time));
-        ADD_VAR_RET("date", timebuf);
+        add_error_variable(connptr, "date", timebuf);
+
+        add_error_variable(connptr, "website", "http://tinyproxy.banu.com/");
+        add_error_variable(connptr, "version", VERSION);
+        add_error_variable(connptr, "package", PACKAGE);
 
         return (0);
 }