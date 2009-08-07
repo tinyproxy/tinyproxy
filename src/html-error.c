76dadf47d3ddf406162b7956f0dc64ba26e2b993
Michael Adam <obnox@samba.org>
2009-08-07 08:51:35 +0200

    make a const string const in send_http_error_message().
    Michael

--- src/html-error.c
+++ src/html-error.c
@@ -192,7 +192,7 @@ send_http_error_message (struct conn_s *connptr)
   char *error_file;
   FILE *infile;
   int ret;
-  char *fallback_error =
+  const char *fallback_error =
     "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
     "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" "
     "\"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n"
