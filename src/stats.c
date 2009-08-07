3127baa4c640c4e87aadbf642febdb43a4822ca1
Michael Adam <obnox@samba.org>
2009-08-07 08:37:44 +0200

    showstats(): add a const to a const string
    Michael

--- src/stats.c
+++ src/stats.c
@@ -63,7 +63,7 @@ init_stats (void)
 int
 showstats (struct conn_s *connptr)
 {
-  static char *msg =
+  static const char *msg =
     "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
     "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" "
     "\"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n"
