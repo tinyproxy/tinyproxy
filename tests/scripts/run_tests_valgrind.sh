6c83a49bbb22ed107919a8410ce660df0ff20968
Michael Adam <obnox@samba.org>
2009-09-11 01:10:11 +0200

    tests: add a script wrapper to run tinyproxy under valgrind in the tests.
    This runs valgrind with the -q switch - i.e. the log file
    tests/env/var/log/valgrind.log will only contain anything when there were
    valgrind errors. (Memory leaks...)
    
    Michael

---
+++ tests/scripts/run_tests_valgrind.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# testsuite runner for tinyproxy : valgrind wrapper
+#
+# Copyright (C) 2009 Michael Adam <obnox@samba.org>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
+
+
+SCRIPTS_DIR=$(dirname $0)
+BASEDIR=$SCRIPTS_DIR/../..
+TESTS_DIR=$SCRIPTS_DIR/..
+TESTENV_DIR=$TESTS_DIR/env
+LOG_DIR=$TESTENV_DIR/var/log
+
+VALGRIND="valgrind -q --tool=memcheck --leak-check=full --log-file=$LOG_DIR/valgrind.log" $SCRIPTS_DIR/run_tests.sh
+
