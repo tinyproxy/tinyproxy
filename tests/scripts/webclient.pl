bc1da1cbb546c62304f717e9a11d620b39b54c7b
Michael Adam <obnox@samba.org>
2009-09-10 12:08:17 +0200

    tests:webclient: add complete GPL boilerplate.
    Michael

--- tests/scripts/webclient.pl
+++ tests/scripts/webclient.pl
@@ -3,9 +3,20 @@
 # Simple command line web client.
 # Initially loosely based on examples from the perlipc manpage.
 #
-# Copyright (C) 2009 Michael Adam
+# Copyright (C) 2009 Michael Adam <obnox@samba.org>
 #
-# License: GPL
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 
