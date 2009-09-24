43d0af85af92c93671dcc7016b58ee42af8c863d
Mukund Sivaraman <muks@banu.com>
2009-09-24 08:16:50 +0530

    Remove obsolete comment

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-# -*- sh -*-
-#
-# Make the Autotools scripts after checking out the source code from CVS.
-# This script was taken from the Autotool Book.  I wonder if autoreconf
-# can now be used...
-#
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
