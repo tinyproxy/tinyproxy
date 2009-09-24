1c72d8f0631401487ed9bd9340fe2269658ccfa9
Mukund Sivaraman <muks@banu.com>
2009-09-24 08:16:26 +0530

    build: Run configure in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -6,8 +6,31 @@
 # can now be used...
 #
 
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+ORIGDIR=`pwd`
+
 set -x
+
+cd $srcdir
+
 aclocal -I m4macros \
   && autoheader \
   && automake --gnu --add-missing \
   && autoconf
+
+cd $ORIGDIR
+
+set -
+
+echo $srcdir/configure --enable-maintainer-mode "$@"
+$srcdir/configure --enable-maintainer-mode "$@"
+RC=$?
+if test $RC -ne 0; then
+  echo
+  echo "Configure failed or did not finish!"
+  exit $RC
+fi
+
+echo
+echo "Now type 'make' to compile Tinyproxy."
