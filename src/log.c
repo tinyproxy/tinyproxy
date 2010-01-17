26b4ab07a41eabbfab74bb3c8d76a23e34d16919
Mukund Sivaraman <muks@banu.com>
2010-01-17 14:27:19 +0530

    Remove extra argument to log_message()

--- src/log.c
+++ src/log.c
@@ -264,7 +264,7 @@ int setup_logging (void)
                         config.syslog = TRUE;
 
                         log_message (LOG_CRIT, "ERROR: Could not create log "
-                                     "file %s: %s.\n", PACKAGE,
+                                     "file %s: %s.\n",
                                      config.logf_name, strerror (errno));
                         log_message (LOG_CRIT,
                                      "Falling back to syslog logging\n");
