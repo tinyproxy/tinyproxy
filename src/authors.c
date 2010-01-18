edea7e37d08e3045f4e7932ed74e09b5b8dc5c5c
Mukund Sivaraman <muks@banu.com>
2010-01-18 19:05:31 +0530

    Show authors and documenters when license is requested

---
+++ src/authors.c
@@ -0,0 +1,41 @@
+
+/* NOTE: This file is auto-generated from authors.xml, do not edit it. */
+
+#include "authors.h"
+
+static const char * const authors[] =
+{
+        "Andrew Stribblehill",
+        "David Shanks",
+        "Jeremy Hinegardner",
+        "Kim Holviala",
+        "Mathew Mrosko",
+        "Matthew Dempsky",
+        "Michael Adam",
+        "Mukund Sivaraman",
+        "Robert James Kaes",
+        "Steven Young",
+        NULL
+};
+
+static const char * const documenters[] =
+{
+        "Marc Silver",
+        "Michael Adam",
+        "Mukund Sivaraman",
+        "Robert James Kaes",
+        "Steven Young",
+        NULL
+};
+
+const char * const *
+authors_get_authors (void)
+{
+        return authors;
+}
+
+const char * const *
+authors_get_documenters (void)
+{
+        return documenters;
+}
--- src/main.c
+++ src/main.c
@@ -32,6 +32,7 @@
 #include "main.h"
 
 #include "anonymous.h"
+#include "authors.h"
 #include "buffer.h"
 #include "conf.h"
 #include "daemon.h"
@@ -92,9 +93,13 @@ display_version (void)
 static void
 display_license (void)
 {
+        const char * const *authors;
+        const char * const *documenters;
+
         display_version ();
 
         printf ("\
+\n\
   Copyright 1998       Steven Young (sdyoung@well.com)\n\
   Copyright 1998-2002  Robert James Kaes (rjkaes@users.sourceforge.net)\n\
   Copyright 1999       George Talusan (gstalusan@uwaterloo.ca)\n\
@@ -114,7 +119,26 @@ display_license (void)
 \n\
   You should have received a copy of the GNU General Public License\n\
   along with this program; if not, write to the Free Software\n\
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.\n");
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.\n\
+\n");
+
+        authors = authors_get_authors ();
+
+        printf ("\nAUTHORS:\n");
+        while (*authors) {
+                printf ("  %s\n", *authors);
+                authors++;
+        }
+
+        documenters = authors_get_documenters ();
+
+        printf ("\nDOCUMENTERS:\n");
+        while (*documenters) {
+                printf ("  %s\n", *documenters);
+                documenters++;
+        }
+
+        printf ("\n");
 }
 
 /*
