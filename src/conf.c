b672ca03fee5813b18354fb5ed221f345f1310e3
Michael Adam <obnox@samba.org>
2010-08-24 22:47:35 +0200

    [BB#91] Fix upstream proxy support.
    Patch by Jordi Mallach.

--- src/conf.c
+++ src/conf.c
@@ -1046,8 +1046,8 @@ static HANDLE_FUNC (handle_upstream)
                 return -1;
         port = (int) get_long_arg (line, &match[7]);
 
-        if (match[9].rm_so != -1) {
-                domain = get_string_arg (line, &match[9]);
+        if (match[10].rm_so != -1) {
+                domain = get_string_arg (line, &match[10]);
                 if (domain) {
                         upstream_add (ip, port, domain, &conf->upstream_list);
                         safefree (domain);
