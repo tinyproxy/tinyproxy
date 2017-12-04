9bb699628fe0375ab03fa795c44261e97a898beb
rofl0r <retnyg@gmx.net>
2017-12-04 11:33:01 +0000

    safe_write/read: take void* buffer for generic use
    if using one of unsigned or signed char for the function prototype, one
    gets nasty warnings when using it with the other type. the only proper
    solution is to put void* into the prototype, and then specialize the pointer
    inside the function using an automatic variable.
    for exactly this reason, libc functions like read(), write(), etc use void*
    too.

--- src/network.h
+++ src/network.h
@@ -21,8 +21,8 @@
 #ifndef TINYPROXY_NETWORK_H
 #define TINYPROXY_NETWORK_H
 
-extern ssize_t safe_write (int fd, const char *buffer, size_t count);
-extern ssize_t safe_read (int fd, char *buffer, size_t count);
+extern ssize_t safe_write (int fd, const void *buf, size_t count);
+extern ssize_t safe_read (int fd, void *buf, size_t count);
 
 extern int write_message (int fd, const char *fmt, ...);
 extern ssize_t readline (int fd, char **whole_buffer);
