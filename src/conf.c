1b26dc11284f73c2b5b0332ca183ecc331c85b24
Vasily <just.one.man@yandex.ru>
2018-11-16 14:17:04 +0300

    Added more liberty to upstream username, too

--- src/conf.c
+++ src/conf.c
@@ -52,7 +52,8 @@
 #define BOOL "(yes|on|no|off)"
 #define INT "((0x)?[[:digit:]]+)"
 #define ALNUM "([-a-z0-9._]+)"
-#define PASSWORD "([^:@]*)"
+#define USERNAME "([^:]*)"
+#define PASSWORD "([^@]*)"
 #define IP "((([0-9]{1,3})\\.){3}[0-9]{1,3})"
 #define IPMASK "(" IP "(/[[:digit:]]+)?)"
 #define IPV6 "(" \
@@ -258,7 +259,7 @@ struct {
         },
         {
                 BEGIN "(upstream)" WS "(http|socks4|socks5)" WS
-                      "(" ALNUM /*username*/ ":" PASSWORD /*password*/ "@" ")?"
+                      "(" USERNAME /*username*/ ":" PASSWORD /*password*/ "@" ")?"
                       "(" IP "|" ALNUM ")"
                       ":" INT "(" WS STR ")?"
                 END, handle_upstream, NULL
