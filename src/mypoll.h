bf5bd24303510533d88b87ed10c97b8ead253b60
rofl0r <rofl0r@users.noreply.github.com>
2020-09-15 20:01:01 +0100

    use poll() where available

--- src/mypoll.h
+++ src/mypoll.h
@@ -1,21 +1,28 @@
 #ifndef MYPOLL_H
 #define MYPOLL_H
 
-#include <sys/select.h>
+#include "config.h"
 
 #ifdef HAVE_POLL_H
+
 #include <poll.h>
 typedef struct pollfd pollfd_struct;
+
+#define MYPOLL_READ POLLIN
+#define MYPOLL_WRITE POLLOUT
+
 #else
+
+#include <sys/select.h>
 typedef struct mypollfd {
 	int   fd;
 	short events;
 	short revents;
 } pollfd_struct;
-#endif
 
 #define MYPOLL_READ (1<<1)
 #define MYPOLL_WRITE (1<<2)
+#endif
 
 int mypoll(pollfd_struct* fds, int nfds, int timeout);
 
