094db9d6703a5077520d80ed70ff66b8f1766d55
rofl0r <rofl0r@users.noreply.github.com>
2020-09-27 15:44:50 +0100

    version.sh: relax regex for release tag detection
    this allows to use tag names with a custom suffix too.

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,7 +5,7 @@ GIT_DIR="${SCRIPT_DIR}/../.git"
 
 if test -d "${GIT_DIR}" ; then
 	if type git >/dev/null 2>&1 ; then
-		gitstr=$(git describe --match '[0-9]*.[0-9]*.[0-9]*' 2>/dev/null)
+		gitstr=$(git describe --match '[0-9]*.[0-9]*.*' 2>/dev/null)
 		if test "x$?" != x0 ; then
 			sed 's/$/-git/' < VERSION
 		else
